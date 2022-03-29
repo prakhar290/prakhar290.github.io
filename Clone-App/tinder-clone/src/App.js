22ea4658106c1dcc6e5949127c122dc6d59cf041
Prakhar Varshney <prakhar292000@gmail.com>
2022-03-29 10:42:14 +0530

    init

---
+++ Clone-App/tinder-clone/src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+import Header from './Header';
+
+function App() {
+  return (
+    //BEM naming convention
+    <div className="app">
+
+      {/*Header*/}
+      <Header />
+      {/*Tinder Cards*/}
+      {/*Swipe buttons*/}
+    </div>
+  );
+}
+
+export default App;
